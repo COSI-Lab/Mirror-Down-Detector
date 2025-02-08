5f4f1f48037f500838ed20f982d67fe8d458437c
Brendan Connelly <brendanjconnelly17@gmail.com>
2025-02-08 01:29:10 -0500

    ping.cpp: Clang-tidy

--- src/ping.cpp
+++ src/ping.cpp
@@ -1,22 +1,22 @@
 #include <cstdlib>
+#include <cstring>
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <regex>
 #include <sstream>
-#include <cstring>
 #include <string>
 #include <utility>
-#include <ctime>
 
-#include <unistd.h>
-#include <sys/wait.h>
-#include <poll.h>
 #include <fcntl.h>
+#include <poll.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "ping.h"
 
-constexpr time_t PING_TIMEOUT_SECONDS = 10;
-constexpr ssize_t PING_BUFFER = 1024;
+constexpr time_t  PING_TIMEOUT_SECONDS = 10;
+constexpr ssize_t PING_BUFFER          = 1024;
 
 /**
  * @return `true` if the URL or IP responds to ping requests, and `false`
@@ -31,16 +31,18 @@ std::pair<bool, std::string> ping(std::string url)
     pid_t pid = fork();
 
     /* Child process */
-    if(pid == 0)
+    if (pid == 0)
     {
         // Using strdup() to avoid const casting
-        char* argv[] = {strdup("/bin/ping"),
-                         strdup(url.c_str()),
-                         strdup("-c"), strdup("3"), // 3 times
-                         strdup("-W"), strdup("1"), // 1s timeout
-                         strdup("-i"), strdup("0.1"), NULL}; // 0.1s interval
+        char* argv[] = {
+            strdup("/bin/ping"), strdup(url.c_str()),
+            strdup("-c"), strdup("3"),      // 3 times
+            strdup("-W"), strdup("1"),      // 1s timeout
+            strdup("-i"), strdup("0.1"),    // 0.1s interval    
+            NULL
+        };                                    
 
-        close(fd[0]); // don't output to console
+        close(fd[0]);   // don't output to console
         dup2(fd[1], 1); // Reassign ping's stdout to our pipe
 
         execve("/bin/ping", argv, NULL);
@@ -48,27 +50,39 @@ std::pair<bool, std::string> ping(std::string url)
         return std::make_pair(false, "execve(2) error " + errno);
     }
 
-    if(pid == -1)
+    if (pid == -1)
     {
         return std::make_pair(false, "Error spawning child.");
     }
-    
+
     // Read ping pipe into buffer
-    char *pingRes = new char[PING_BUFFER]{}; // 1024 bytes ought to be enough?
+    char* pingRes = new char[PING_BUFFER] {}; // 1024 bytes ought to be enough?
 
     int flags = fcntl(fd[0], F_GETFL);
-    int rc = fcntl(fd[0], F_SETFL, O_NONBLOCK | flags);
+    int rc    = fcntl(fd[0], F_SETFL, O_NONBLOCK | flags);
 
-    time_t end = time(NULL) + PING_TIMEOUT_SECONDS;
-    int pingResPtr = 0;
+    time_t end        = time(NULL) + PING_TIMEOUT_SECONDS;
+    int    pingResPtr = 0;
 
     int childStatus;
-    while(time(NULL) < end) {
-        ssize_t bytesRead = read(fd[0], pingRes + pingResPtr, PING_BUFFER-pingResPtr);
-        if(bytesRead == 0) break; // EOF
-        if(bytesRead == -1 && (errno != EAGAIN || errno != EWOULDBLOCK)) break;
-        if(bytesRead > 0) pingResPtr += bytesRead;
-        if(waitpid(pid, &childStatus, WNOHANG) == pid) {
+    while (time(NULL) < end)
+    {
+        ssize_t bytesRead
+            = read(fd[0], pingRes + pingResPtr, PING_BUFFER - pingResPtr);
+        if (bytesRead == 0)
+        {
+            break; // EOF
+        }
+        if (bytesRead == -1 && (errno != EAGAIN || errno != EWOULDBLOCK))
+        {
+            break;
+        }
+        if (bytesRead > 0)
+        {
+            pingResPtr += bytesRead;
+        }
+        if (waitpid(pid, &childStatus, WNOHANG) == pid)
+        {
             pid = 0; // signal to the outside of the loop that we reaped
             break;
         }
@@ -76,17 +90,18 @@ std::pair<bool, std::string> ping(std::string url)
         sleep(0.5); // lets not hog the cpu
     }
 
-    if(pid != 0) { // we need to clean up the zombie
+    if (pid != 0)
+    { // we need to clean up the zombie
         kill(pid, SIGKILL);
         waitpid(pid, NULL, 0);
     }
 
-    std::string pingStr{pingRes};
+    std::string pingStr { pingRes };
 
     // TODO: change this to check for < 100% packet loss
     std::regex expression("(3 packets transmitted, 3 received?)");
     std::regex expression2("(3 packets transmitted, 2 received?)");
-    bool up = std::regex_search(pingStr, expression)
+    bool       up = std::regex_search(pingStr, expression)
            || std::regex_search(pingStr, expression2);
 
     // return a status and the ping output
