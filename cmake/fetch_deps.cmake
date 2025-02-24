4900ae244db3f956a711693ebaa8e9f710b90c0e
carykees98 <trainboy980@gmail.com>
2025-02-24 22:25:41 +0000

    added json library and configured some dpp options

--- cmake/fetch_deps.cmake
+++ cmake/fetch_deps.cmake
@@ -1,5 +1,11 @@
 include(FetchContent)
 
+FetchContent_Declare(json
+    GIT_REPOSITORY https://github.com/nlohmann/json
+    GIT_TAG v3.11.3
+)
+FetchContent_MakeAvailable(json)
+
 FetchContent_Declare(dpp
     GIT_REPOSITORY https://github.com/brainboxdotcc/DPP
     GIT_TAG v10.1.0
